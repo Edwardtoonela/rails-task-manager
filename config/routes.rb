4fd63f143096262c38e9ebe1a7b106f434ae58f5
Edward Toonela <edward.toonela@icloud.com>
2022-02-22 14:14:56 -0300

    added task model + enabled user task listing through

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
